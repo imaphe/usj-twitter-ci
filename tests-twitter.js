64f7da0c5ce5e8969c031a206b5bf11790ef0d4a
Daniel Coloma <danielcoloma@gmail.com>
2022-05-19 15:59:27 +0200

    Add test for two mp4 videos

--- tests-twitter.js
+++ tests-twitter.js
@@ -17,6 +17,27 @@ oneMp4 = [{
   }
 ];
 
+twoMp4s = [{
+    content_type: 'application/x-mpegUrl',
+    url: 'https://video.twimg.com/video1.mp4'
+  },
+  {
+    content_type: 'application/x-mpegURL',
+    url: 'https://video.twimg.com/video2.mp4'
+  },
+  {
+    bitrate: 42001,
+    content_type: 'video/mp4',
+    url: 'https://video.twimg.com/video3.mp4'
+  },
+  {
+    bitrate: 42000,
+    content_type: 'video/mp4',
+    url: 'https://video.twimg.com/video4.mp4'
+  }
+];
+
 test("getBestVideo", function(assert) {
   assert.equal(getBestVideo(oneMp4), 'https://video.twimg.com/video4.mp4', '4_videos_one_mp4_last_one');
+  assert.equal(getBestVideo(twoMp4s), 'https://video.twimg.com/video3.mp4', '4_videos_two_mp4s_third_one');
 });
