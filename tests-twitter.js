9fe1f0754676e9b51ab957eeb2ee2d0404489b85
Daniel Coloma <danielcoloma@gmail.com>
2022-05-19 12:27:33 +0200

    First commit

---
+++ tests-twitter.js
@@ -0,0 +1,22 @@
+oneMp4 = [{
+    content_type: 'application/x-mpegUrl',
+    url: 'https://video.twimg.com/video1.mp4'
+  },
+  {
+    content_type: 'application/x-mpegURL',
+    url: 'https://video.twimg.com/video2.mp4'
+  },
+  {
+    content_type: 'application/x-mpegUrl',
+    url: 'https://video.twimg.com/video3.mp4'
+  },
+  {
+    bitrate: 432000,
+    content_type: 'video/mp4',
+    url: 'https://video.twimg.com/video4.mp4'
+  }
+];
+
+test("getBestVideo", function(assert) {
+  assert.equal(getBestVideo(oneMp4), 'https://video.twimg.com/video4.mp4', '4_videos_one_mp4_last_one');
+});
