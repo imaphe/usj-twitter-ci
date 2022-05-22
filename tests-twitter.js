f586b5172a115153fb6dbaddee944c4a880afb2d
anitajarque <49341192+anitajarque@users.noreply.github.com>
2022-05-22 12:11:21 +0200

    New test cases
    I’ve done 8 test. Divided in 3 big areas: bitrate, type and list.
    
    In bitrate I separate between max Bitrate in “sameBitrate_done”  is checking a test where 2 of the 4rd videos are with the same Bitrate. In “plus_1_bitrate_done”  is checking the that one of the video has just 1 bitrate more than the other. “less_1_bitrate_done” is checking the opposite case of the  “plus_1_bitrate_done”. “bitrate_negative_done” is checking where one of those videos is negative. Last one in this area is “NaN_mp4” is checking where one of the videos don’t have bitrate instead of one that have it.
    
    The next area is type where the first one is “not_MP4_done” were either of the videos are MP4. The “max_Bitrate_notMP4_done”  when one has the biggest bitrate but is not an MP4 video. 
    
    Last area is list where there is no video list.

--- tests-twitter.js
+++ tests-twitter.js
@@ -1,5 +1,90 @@
-oneMp4 = [{
-    content_type: 'application/x-mpegUrl',
+equalBitrateMp4 = [{
+
+  bitrate: 42000,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video1.mp4'
+},
+{
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video2.mp4'
+},
+{
+  content_type: 'application/x-mpegUrl',
+  url: 'https://video.twimg.com/video3.mp4'
+},
+{
+  bitrate: 42000,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video4.mp4'
+}
+];
+
+plus1BitrateMp4 = [{
+
+  bitrate: 42001,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video1.mp4'
+},
+{
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video2.mp4'
+},
+{
+  content_type: 'application/x-mpegUrl',
+  url: 'https://video.twimg.com/video3.mp4'
+},
+{
+  bitrate: 42000,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video4.mp4'
+}
+];
+
+less1BitrateMp4 = [{
+
+  bitrate: 41999,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video1.mp4'
+},
+{
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video2.mp4'
+},
+{
+  content_type: 'application/x-mpegUrl',
+  url: 'https://video.twimg.com/video3.mp4'
+},
+{
+  bitrate: 42000,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video4.mp4'
+}
+];
+
+
+bitrateNegativeMp4=[{
+  bitrate: 41999,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video1.mp4'
+},
+{
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video2.mp4'
+},
+{
+  content_type: 'application/x-mpegUrl',
+  url: 'https://video.twimg.com/video3.mp4'
+},
+{
+  bitrate: -42000,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video4.mp4'
+}
+];
+
+naNMp4 = [{
+    bitrate: 42000,
+    content_type: 'video/mp4',
     url: 'https://video.twimg.com/video1.mp4'
   },
   {
@@ -11,12 +96,67 @@ oneMp4 = [{
     url: 'https://video.twimg.com/video3.mp4'
   },
   {
-    bitrate: 42000,
+    bitrate: NaN,
     content_type: 'video/mp4',
     url: 'https://video.twimg.com/video4.mp4'
   }
 ];
 
-test("getBestVideo", function(assert) {
-  assert.equal(getBestVideo(oneMp4), 'https://video.twimg.com/video4.mp4', '4_videos_one_mp4_last_one');
+
+test("bitrate", function(assert) {
+  assert.equal(getBestVideo(equalBitrateMp4), 'https://video.twimg.com/video1.mp4', 'sameBitrate_done');
+  assert.equal(getBestVideo(plus1BitrateMp4), 'https://video.twimg.com/video1.mp4', 'plus_1_bitrate_done');
+  assert.equal(getBestVideo(less1BitrateMp4), 'https://video.twimg.com/video4.mp4', 'less_1_bitrate_done');
+  assert.equal(getBestVideo(bitrateNegativeMp4), 'https://video.twimg.com/video1.mp4', 'bitrate_negative_done');
+  assert.equal(getBestVideo(naNMp4), 'https://video.twimg.com/video1.mp4', 'NaN_mp4');
+});
+
+maxBitrateNotMp4=[{
+  bitrate: 41999,
+  content_type: 'video/mp4',
+  url: 'https://video.twimg.com/video1.mp4'
+},
+{
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video2.mp4'
+},
+{
+  content_type: 'application/x-mpegUrl',
+  url: 'https://video.twimg.com/video3.mp4'
+},
+{
+  bitrate: 42000,
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video4.mp4'
+}
+];
+
+notMp4=[{
+  bitrate: 41999,
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video1.mp4'
+},
+{
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video2.mp4'
+},
+{
+  content_type: 'application/x-mpegUrl',
+  url: 'https://video.twimg.com/video3.mp4'
+},
+{
+  bitrate: 42000,
+  content_type: 'application/x-mpegURL',
+  url: 'https://video.twimg.com/video4.mp4'
+}
+];
+
+test("type", function(assert) {
+assert.equal(getBestVideo(notMp4), null, 'not_MP4_done');
+assert.equal(getBestVideo(maxBitrateNotMp4), 'https://video.twimg.com/video1.mp4', 'max_Bitrate_notMP4_done');
+});
+
+notVideos=[]
+test("list", function(assert) {
+assert.equal(getBestVideo(notVideos), null, 'not_videos_done');
 });
